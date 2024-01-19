09ab2c06a0c3958545aff6c0ba300972eff5a314
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-19 13:13:50 +0200

    Implementation of the class with the functions of calling the api's

---
+++ lib/data/datasources/remote/movie_remote_data_source_impl.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:movie_app/data/datasources/movie_remote_data_source.dart';
+import 'package:movie_app/data/models/movie_model.dart';
+import '../../../core/errors/server_exception.dart';
+
+class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
+  final http.Client client;
+
+  MovieRemoteDataSourceImpl({required this.client});
+  static const BASE_URL = "https://api.themoviedb.org/3";
+  static const API_KEY = "47d6b785987e7caa9a120e600c6d7632";
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/movie/popular?api_key=$API_KEY"),
+    );
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getTrendingMovies() async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/trending/movie/day?api_key=$API_KEY"),
+    );
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> searchMovies(String query) async {
+    final response = await client.get(
+      Uri.parse("$BASE_URL/search/movie?query=$query&api_key=$API_KEY"),
+    );
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final List<MovieModel> movies = (responseBody['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw ServerException();
+    }
+  }
+}
