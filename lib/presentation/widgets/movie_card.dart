0a339059d37e87008519bb22ebc57fd64bdc01ec
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-22 19:56:22 +0200

    Creating the movie card widget

---
+++ lib/presentation/widgets/movie_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/domain/entities/Movie.dart';
+
+class MovieCard extends StatelessWidget {
+  MovieCard(this.movie, {super.key});
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black87,
+      elevation: 2,
+      margin: const EdgeInsets.all(5),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            width: 130,
+            child: Ink.image(
+              image: NetworkImage(
+                  'https://image.tmdb.org/t/p/w500/${movie.posterPath}'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            movie.title,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 14.0,
+              fontFamily: 'NotoSans',
+              color: Colors.grey,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
