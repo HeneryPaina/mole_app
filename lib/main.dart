d1c2474410ed1d1f9b8e66e118b2ee703b7aa079
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-22 14:42:24 +0200

    update the main class

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:movie_app/presentation/bloc/popular_movies/popular_movies_event.
 import 'package:movie_app/presentation/bloc/search_movies/search_movies_bloc.dart';
 import 'package:movie_app/presentation/bloc/trending_movies/trending_movies_bloc.dart';
 import 'package:movie_app/presentation/bloc/trending_movies/trending_movies_event.dart';
-import 'package:movie_app/presentation/pages/home_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/presentation/widgets/bottom_navigation_bar.dart';
 
 void main() {
   init();
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
             create: (context) => getIt<SearchMoviesBloc>(),
           ),
         ],
-        child: HomeScreen(),
+        child: MyBottomNavigationBar(),
       ),
     );
   }
