8e890817d37791676ef49127bcd4686c2c4d2be1
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-22 13:23:29 +0200

    Creating the state management classes with the Bloc

---
+++ lib/presentation/bloc/search_movies/search_movies_state.dart
@@ -0,0 +1,18 @@
+import '../../../domain/entities/Movie.dart';
+
+abstract class SearchMoviesState {}
+
+class SearchMoviesInitial extends SearchMoviesState {}
+
+class SearchMoviesLoading extends SearchMoviesState {}
+
+class SearchMoviesLoaded extends SearchMoviesState {
+  final List<Movie> movies;
+  SearchMoviesLoaded(this.movies);
+}
+
+class SearchMoviesError extends SearchMoviesState {
+  final String message;
+
+  SearchMoviesError(this.message);
+}
