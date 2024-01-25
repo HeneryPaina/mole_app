240c4a51c1390fb4fddbaf8b531791357e38bab9
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-25 14:44:04 +0200

    Create appbar

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -12,6 +12,24 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 10, 8, 10),
+          child: Image.asset(
+            'assets/img/logo.png',
+            fit: BoxFit.contain,
+            height: 60,
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.search,
+            size: 28,
+            color: Colors.white,
+          ),
+        ],
+      ),
     );
   }
 }
