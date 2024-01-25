553c2e86b909146cdd62c28bbcf546c95c649b6e
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-25 14:47:38 +0200

    Adding the widget for the carousel slide with the movies

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/popular_movies/popular_movies_bloc.dart';
+import '../bloc/popular_movies/popular_movies_state.dart';
+import '../widgets/carousel_slider_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,6 +33,54 @@ class _HomeScreenState extends State<HomeScreen> {
             size: 28,
             color: Colors.white,
           ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Trending",
+                        style: TextStyle(
+                            fontFamily: 'NotoSans', color: Colors.white),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                       },
+                      child: Text(
+                        "Popular",
+                        style: TextStyle(
+                            fontFamily: 'NotoSans', color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
+                builder: (context, state) {
+                  if (state is PopularMoviesLoaded) {
+                    // Verifica se o estado é do tipo PopularMoviesLoaded
+                    return CarouselWidget(movies: state.movies);
+                  } else {
+                    // Estado inicial, de carregamento ou de erro
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ],
+          ),
+
         ],
       ),
     );
