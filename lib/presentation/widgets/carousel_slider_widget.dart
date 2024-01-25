7eba4fe671276d0f6f490c67c641ff3735ab1ae4
Henery Paina <44199686+HeneryPaina@users.noreply.github.com>
2024-01-25 14:41:16 +0200

    Update ui for movie card and add navigator on press button

--- lib/presentation/widgets/carousel_slider_widget.dart
+++ lib/presentation/widgets/carousel_slider_widget.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import '../../domain/entities/Movie.dart';
+import '../pages/movies_details_screen.dart';
 
 class CarouselWidget extends StatelessWidget {
   final List<Movie> movies;
@@ -9,10 +10,9 @@ class CarouselWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final List<Movie> moviesToDisplay = movies.take(10).toList();
     return CarouselSlider(
       options: CarouselOptions(
-        height: 456.5,
+        height: 400,
         enlargeCenterPage: true,
         autoPlay: true,
         autoPlayInterval: Duration(seconds: 3),
@@ -23,25 +23,28 @@ class CarouselWidget extends StatelessWidget {
           builder: (BuildContext context) {
             return Column(
               children: [
-                Card(
-                  elevation: 2,
-                  margin: const EdgeInsets.all(5),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: SizedBox(
-                    height: 350,
-                    width: 280,
-                    child: Ink.image(
-                      image: NetworkImage(
-                          'https://image.tmdb.org/t/p/w200/${movie.posterPath}'),
-                      fit: BoxFit.cover,
+                Expanded(
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 2,
+                    margin: const EdgeInsets.all(5),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: SizedBox(
+                      height: 300,
+                      width: 200,
+                      child: Ink.image(
+                        image: NetworkImage(
+                            'https://image.tmdb.org/t/p/w200/${movie.posterPath}'),
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 1),
+                  padding: const EdgeInsets.only(top: 11, left: 28, right: 28),
                   child: Text(
                     movie.title,
                     maxLines: 1,
@@ -53,7 +56,33 @@ class CarouselWidget extends StatelessWidget {
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red, // Cor de fundo do botão
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetailsScreen(movie: movie),
+                        ),
+                      );
+                    },
+                    icon: Icon(
+                      // <-- Icon
+                      Icons.info_outline, color: Colors.white,
+                      size: 24.0,
+                    ),
+                    label: Text(
+                      'Details',
+                      style:
+                          TextStyle(color: Colors.white, fontFamily: 'NotoSans'),
+                    ), // <-- Text
+                  ),
+                ),
               ],
             );
           },
